749618d9f629799adc4523d54b24a973183fb03d
umerkanavi12 <139278618+umerkanavi12@users.noreply.github.com>
2024-09-11 23:27:23 +0530

    Update productController.js

--- controllers/productController.js
+++ controllers/productController.js
@@ -323,6 +323,26 @@ export const realtedProductController = async (req, res) => {
   }
 };
 
+//productCategoryController
+export const productCategoryController = async (req, res) => {
+  try {
+    const category = await categoryModel.findOne({ slug: req.params.slug });
+    const products = await productModel.find({ category }).populate("category");
+    res.status(200).send({
+      success: true,
+      category,
+      products,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(400).send({
+      success: false,
+      error,
+      message: "Error While Getting products",
+    });
+  }
+};
+
 // Get Braintree Token Controller
 export const braintreeTokenController = async (req, res) => {
   try {
